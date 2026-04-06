b8333332e6e1e37a1e78059311d092f484c1788f
arpbabay <arpibabajan1188@gmail.com>
2026-04-06 23:30:55 +0400

    new

---
+++ push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arpbabay <arpbabay@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/06 22:22:15 by arpbabay          #+#    #+#             */
+/*   Updated: 2026/04/06 22:25:37 by arpbabay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# define PUSH_SWAP_H
+
+# include <limits.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdint.h>
+
+int     isnum(char *str);
+long    atol(const char *nptr);
