acca53f490e3fcfd3381e0c8ef99904426d7a5dd
arpbabay <arpibabajan1188@gmail.com>
2026-04-07 17:35:46 +0400

    h

--- push_swap.h
+++ push_swap.h
@@ -3,11 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD
-/*   By: nagharib <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: arpbabay <arpbabay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 18:59:35 by nagharib          #+#    #+#             */
-/*   Updated: 2026/04/06 19:47:07 by nagharib         ###   ########.fr       */
+/*   Updated: 2026/04/07 17:35:35 by arpbabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +26,11 @@ typedef struct s_node
 t_node	*new_node(int value);
 void	add_back(t_node **head, t_node *new);
 
+
 t_node	*build_stack(int *arr, int size);
 void	free_list(t_node *head);
-#endif
-=======
-/*   By: arpbabay <arpbabay@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/06 22:22:15 by arpbabay          #+#    #+#             */
-/*   Updated: 2026/04/06 22:25:37 by arpbabay         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-# define PUSH_SWAP_H
-
-# include <limits.h>
-# include <stddef.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <stdint.h>
 
 int     isnum(char *str);
 long    atol(const char *nptr);
->>>>>>> b8333332e6e1e37a1e78059311d092f484c1788f
+
+#endif
