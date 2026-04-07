8ec5be70918b4b5aadff70b977eebea260010fa2
Nairi <nairigharibyan06@gmail.com>
2026-04-07 19:45:12 +0400

    sa sb

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: arpbabay <arpbabay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 18:59:35 by nagharib          #+#    #+#             */
-/*   Updated: 2026/04/07 17:35:35 by arpbabay         ###   ########.fr       */
+/*   Updated: 2026/04/07 19:44:37 by nagharib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,10 @@ typedef struct s_node
 
 t_node	*new_node(int value);
 void	add_back(t_node **head, t_node *new);
-
-
 t_node	*build_stack(int *arr, int size);
 void	free_list(t_node *head);
-
 int     isnum(char *str);
 long    atol(const char *nptr);
-
+// sa
+// sb
 #endif
