1f766a601bd86fc957caf1ca7bca82c4abbc1faa
arpbabay <arpibabajan1188@gmail.com>
2026-04-07 20:15:58 +0400

    new main

--- main.c
+++ main.c
@@ -6,33 +6,24 @@
 /*   By: arpbabay <arpbabay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 22:22:27 by arpbabay          #+#    #+#             */
-/*   Updated: 2026/04/06 23:30:26 by arpbabay         ###   ########.fr       */
+/*   Updated: 2026/04/07 18:57:10 by arpbabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int     i;
-    int     j;
-    char    **split_array;
-    int     checkednum;
+    t_node	*stack_a;
 
-    i = 1;
-    while(i < argc)
+    if (argc < 2)
+        return (0);
+    stack_a = parse_arguments(argc, argv);
+    if (!stack_a)
     {
-        split_array = ft_split(argv[i], ' ');
-        i++;
-        j = 0;
-        while(split_array[j])
-        {
-            if (isnum(split_array[j]))
-            {
-                checkednum = atol(split_array[j]);
-                if (checkednum < INT_MIN || chekedum > INT_MAX)
-                        write(2, "error\n", 7);
-                    return (0);
-            }
-            j++;
-        }
+        write(2, "Error\n", 6);
+        return (1);
     }
+    // disorderi stugum
+    // solve_push_swap(stack_a); 
+    free_list(stack_a);
+    return (0);
 }
--- validator.c
+++ validator.c
@@ -6,7 +6,7 @@
 /*   By: arpbabay <arpbabay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 22:22:36 by arpbabay          #+#    #+#             */
-/*   Updated: 2026/04/06 22:22:38 by arpbabay         ###   ########.fr       */
+/*   Updated: 2026/04/07 18:52:40 by arpbabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,4 +51,87 @@ long	atol(const char *nptr)
 		++nptr;
 	}
 	return ((res) * (sign));
+}
+void	free_split(char **str)
+{
+	int		k;
+
+	if (!str)
+		return ;
+	
+	k = 0;
+	while (str[k])
+	{
+		free(str[k]);
+		k++;
+	}
+	free(str);
+}
+int	is_valid_input(char *str, t_node *stack_a)
+{
+	long	num;
+
+	if (!isnum(str))
+		return (0);
+	
+	num = atol(str);
+	if (num < -2147483648 || num > 2147483647)
+		return (0);
+	
+	if (check_duplicate(stack_a, (int)num))
+		return (0);
+	return (1);
+}
+int check_duplicate(t_node *head, long num)
+{
+    t_node *tmp;
+
+    tmp = head;
+    while (tmp)
+    {
+        if (tmp->value == (int)num)
+            return (1);
+        tmp = tmp->next;
+    }
+    return (0);
+}
+t_node	*parse_arguments(int argc, char **argv)
+{
+    t_node	*stack_a;
+    int		i;
+
+    stack_a = NULL;
+    i = 1;
+    while (i < argc)
+    {
+        if (!process_argument(argv[i], &stack_a))
+        {
+            free_list(stack_a);
+            return (NULL);
+        }
+        i++;
+    }
+    return (stack_a);
+}
+static int	process_argument(char *arg, t_node **stack_a)
+{
+    char	**split_array;
+    int		j;
+
+    split_array = ft_split(arg, ' ');
+    if (!split_array)
+        return (0);
+    j = 0;
+    while (split_array[j])
+    {
+        if (!is_valid_input(split_array[j], *stack_a))
+        {
+            free_split(split_array);
+            return (0);
+        }
+        add_back(stack_a, new_node((int)atol(split_array[j])));
+        j++;
+    }
+    free_split(split_array);
+    return (1);
 }
